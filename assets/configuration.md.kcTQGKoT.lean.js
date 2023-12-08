0a822a18ec2e190fdedee3322eb109e5b8e70acb
delav <delav@users.noreply.github.com>
2023-12-08 06:45:03 +0000

    deploy: f1324e2cac60c6aa55890799ec90f9024b63a0ac

---
+++ assets/configuration.md.kcTQGKoT.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as i,R as n}from"./chunks/framework.7M7mM_5n.js";const o=JSON.parse('{"title":"配置","description":"","frontmatter":{},"headers":[],"relativePath":"configuration.md","filePath":"configuration.md"}'),e={name:"configuration.md"},p=n("",22),l=[p];function t(h,r,k,d,c,E){return a(),i("div",null,l)}const g=s(e,[["render",t]]);export{o as __pageData,g as default};
